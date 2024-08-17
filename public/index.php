d183359600b8e56917984ec628e05e68a37703a8
shunsuke1027 <Calt.nkt.2575@Gmail.com>
2024-08-17 11:03:15 +0900

    8/17/11:03

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex" />
+    <title>ひとこと掲示板</title>
+    <link rel="stylesheet" href="./assets/main.css" />
+</head>
+
+<body>
+    <div class="page-cover">
+        <p class="page-title">ひとこと掲示板</p>
+            <div class="form-cover">
+                <!--投稿入力フォーム-->
+            </div>
+        <hr class="page-divider" />
+        <div class="message-list-cover">
+            <!--投稿内容のリスト表示-->
+        </div>
+        <hr class="page-divider" />
+    </div>
+
+</body>
+
+</html>
