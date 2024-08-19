a98d263df1224d1f756f07d164984290845ab5a0
shunsuke1027 <Calt.nkt.2575@Gmail.com>
2024-08-19 17:53:06 +0900

    8/19 17:52

--- public/index.php
+++ public/index.php
@@ -12,14 +12,42 @@
 <body>
     <div class="page-cover">
         <p class="page-title">ひとこと掲示板</p>
-            <div class="form-cover">
-                <!--投稿入力フォーム-->
-            </div>
         <hr class="page-divider" />
+        <div class="form-cover">
+            <form action="/" method="post">
+            <div class="form-input-title">投稿者ニックネーム</div>
+                <input type="text" name="author_name" maxlength="40" value="" class="input-author-name" />
+                <div class="form-input-error">
+                </div>
+                <div class="form-input-title">投稿内容<small>(必須)</small></div>
+                <textarea name="message" class="input-message"></textarea>
+                <div class="form-input-error">
+                </div>
+                <input type="hidden" name="action_type" value="insert" />
+                <button type="submit" class="input-submit-button">投稿する</button>
+            </div>
+            </form>
+        <hr class="page-divider" />
+        </div>
         <div class="message-list-cover">
             <!--投稿内容のリスト表示-->
+            <small>
+                1件の投稿
+            </small>
+            <div class="message-items">
+                <div class="message-title">
+                    <div>イチロー</div>
+                    <small>2022-01-01 00:00:00</small>
+                    <div class="spacer"></div>
+                    <form action="/" method="post" style="text-align:right">
+                        <input type="hidden" name="id" value="" />
+                        <input type="hidden" name="action_type" value="delete" />
+                        <button type="submit" class="message-delete-button">削除</button> 
+                    </form>
+                </div>
+                <p class="message-line">明けましておめでとうございます</p>
+            </div>
         </div>
-        <hr class="page-divider" />
     </div>
 
 </body>
